aa3b97e35ba2faf788a18348a98a09902ee4fa86
Christiananemtanu <christiana.nemtanu@gmail.com>
2022-01-20 19:52:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP - May the Force be with you</title>
 </head>
 <body>
-
-    <h1> <?php echo "May the Force be with you"; ?> </h1>
+    <h1> <?php echo "May the Force be with you";?> </h1>
 </body>
 </html>
 
