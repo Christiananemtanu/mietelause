7a167d2cdeab5d50df5d243c7b5902cd25b6ad2f
Christiananemtanu <christiana.nemtanu@gmail.com>
2022-01-21 18:42:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - May the Force be with you</title>
+    <title>Mietelause</title>
+    <style> 
+     body {background-color: black;
+    } 
+     h1 {
+    color: blue;
+    }
+    p {
+    color: lightgray;
+    }
+
+    </style>
 </head>
 <body>
-    <h1> <?php echo 'May the Force be with you';?> </h1>
+    <h1> <?php echo '"May the Force be with you"';?> </h1>
+    <p><?php echo "Han Solo, Star Wars";?></p>
+    <p><?php echo date ("d.m.Y");?></p>
 </body>
 </html>
+
